c36e8ef852c1effda38ac8b435634ddbd50193d6
Christophe Forycki <forycki.christophe@gmail.com>
2017-04-11 20:53:23 +0200

    Added Frame entity usage and underscore

--- src/components/game/GameDetails.tsx
+++ src/components/game/GameDetails.tsx
@@ -1,23 +1,27 @@
-import * as React from "react";
+import * as React from 'react';
+import * as _ from 'underscore';
 import {FrameDetails} from '../frame/FrameDetails';
+import {Frame} from '../frame/Frame';
 
 export interface GameDetailsProps {
 
 }
 
 export class GameDetails extends React.Component<GameDetailsProps, undefined> {
+    frames: Array<Frame>;
+
+    constructor() {
+        super();
+        this.frames = [];
+        _.times(10, (i: number) => this.frames.push(_.extend(new Frame(), {number: i + 1})));
+    }
+
     render() {
-        let frames = [];
-        for (let i = 1; i <= 10; ++i) {
-            frames.push({
-                number: i
-            });
-        }
         return (
             <div className="game">
                 {
-                    frames.map((frame) => {
-                        return <FrameDetails frame={frame}/>
+                    this.frames.map((frame) => {
+                        return <FrameDetails key={frame.number} frame={frame}/>
                     })
                 }
             </div>
