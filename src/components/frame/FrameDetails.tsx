699860670938217e4581ccb7026e8840ef18b9dd
Christophe Forycki <forycki.christophe@gmail.com>
2017-05-01 12:56:03 +0200

    FrameDetails refactoring

--- src/components/frame/FrameDetails.tsx
+++ src/components/frame/FrameDetails.tsx
@@ -8,14 +8,14 @@ export interface FrameDetailsProps {
 export class FrameDetails extends React.Component<FrameDetailsProps, undefined> {
     render() {
         const firstThrow = () => {
-            let currentThrow = this.props.frame.throws[0];
+            let currentThrow = Number.isInteger(this.props.frame.throws[0]) ? this.props.frame.throws[0] : '';
             let formattedThrow = '' + currentThrow;
             if (currentThrow === 0) {
                 formattedThrow = '-';
             } else if (currentThrow === 10) {
                 formattedThrow = 'X';
             }
-            if (this.props.frame.number < 10 && currentThrow === 10) {
+            if (this.props.frame.number < 9 && currentThrow === 10) {
                 return <div className="throw"><span>&nbsp;</span></div>;
             }
             return <div className="throw"><span>{formattedThrow}</span></div>;
@@ -24,45 +24,42 @@ export class FrameDetails extends React.Component<FrameDetailsProps, undefined> 
         const secondThrow = () => {
             let previousThrow = this.props.frame.throws[0];
             let currentThrow = this.props.frame.throws[1];
-            let formattedThrow = '' + currentThrow;
-            if (currentThrow === 0) {
-                formattedThrow = '-';
-            } else if (previousThrow === 10 && this.props.frame.number !== 10) {
-                formattedThrow = 'X';
-            } else if (previousThrow !== 10 && (previousThrow + currentThrow) === 10) {
-                formattedThrow = '/';
-            } else if (currentThrow === 10) {
-                formattedThrow = 'X';
-            }
+            let formattedThrow = this.getFormattedThrow(previousThrow, currentThrow);
             return <div className="throw"><span>{formattedThrow}</span></div>;
         };
 
         const thirdThrow = () => {
             let throws = this.props.frame.throws;
             let formattedThrow = '';
-            if (throws && throws.length === 3 && this.props.frame.number === 10) {
-                let previousThrow = throws[1];
+            if (throws && throws.length === 3 && this.props.frame.number === 9) {
+                let previousThrow = throws[1] + throws[0];
                 let currentThrow = throws[2];
-                formattedThrow = '' + currentThrow;
-                if (currentThrow === 0) {
-                    formattedThrow = '-';
-                } else if (previousThrow !== 10 && (previousThrow + currentThrow) === 10) {
-                    formattedThrow = '/';
-                } else if (currentThrow === 10) {
-                    formattedThrow = 'X';
-                }
+                formattedThrow = this.getFormattedThrow(previousThrow, currentThrow);
             }
-            return this.props.frame.number === 10 && <div className="throw"><span>{formattedThrow}</span></div>;
+            return this.props.frame.number === 9 && <div className="throw"><span>{formattedThrow}</span></div>;
         };
 
         return (
             <div className="frame">
-                <div className="number"><span>{this.props.frame.number}</span></div>
+                <div className="number"><span>{this.props.frame.number + 1}</span></div>
                 {firstThrow()}
                 {secondThrow()}
                 {thirdThrow()}
                 <div className="total"><span>{this.props.frame.total}</span></div>
             </div>
         );
+    }
+
+    private getFormattedThrow(previousThrow: number, currentThrow: number): string {
+        let formattedThrow: string = '' + (currentThrow || '');
+        let previous: number = previousThrow >= 10 ? previousThrow - 10 : previousThrow;
+        if (currentThrow === 0) {
+            formattedThrow = '-';
+        } else if (previousThrow !== 10 && (previous + currentThrow) === 10) {
+            formattedThrow = '/';
+        } else if (previousThrow === 10 && this.props.frame.number !== 9 || currentThrow === 10) {
+            formattedThrow = 'X';
+        }
+        return formattedThrow;
     }
 }
